ef20b5c5404f071394a02ac2098305b780fc25e5
okabe@home <okabe_m@hmi.aitech.ac.jp>
2022-12-18 17:36:18 +0900

    first commit

---
+++ CS8/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CS8 {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
