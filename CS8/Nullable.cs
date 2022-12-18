d98fd5a61c4017dd52a859bcf924a1e2f3c9583a
okabe@home <okabe_m@hmi.aitech.ac.jp>
2022-12-18 18:50:05 +0900

    プロジェクトの設定

---
+++ CS8/Nullable.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+// プロジェクトの設定をしてもわざわざこちらでも enable にする必要がある
+#nullable enable 
+
+namespace CS8 {
+    [TestClass]
+    public class Nullable {
+        [TestMethod]
+        public void TestMethod1() {
+            string? a = null;
+            string b = a;
+        }
+    }
+}
