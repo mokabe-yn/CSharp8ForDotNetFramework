6bd06916ebabb6cf5592aa06ffb5d5549d3e3982
okabe@home <okabe_m@hmi.aitech.ac.jp>
2022-12-23 03:11:21 +0900

    Range実装

--- CS8.cs
+++ CS8.cs
@@ -82,7 +82,7 @@ namespace System {
 
         // core method
         public int GetOffset(int length) {
-            if(IsFromEnd) {
+            if (IsFromEnd) {
                 return length - Value;
             } else {
                 return _value;
@@ -110,5 +110,97 @@ namespace System {
         }
         // operators
         public static implicit operator Index(int value) => FromStart(value);
+    }
+
+    internal readonly struct Range : IEquatable<Range> {
+        public Range(Index start, Index end) {
+            Start = start;
+            End = end;
+        }
+
+        // properties
+        public Index Start { get; }
+        public Index End { get; }
+        public static Range All => new Range(Index.Start, Index.End);
+
+        // core method
+        public (int Offset, int Length) GetOffsetAndLength(int length) {
+            int start = Start.GetOffset(length);
+            int end = End.GetOffset(length);
+            int l = end - start;
+
+            if (start > length) throw new ArgumentOutOfRangeException();
+            if (end > length) throw new ArgumentOutOfRangeException();
+            if (l < 0) throw new ArgumentOutOfRangeException();
+
+            return (start, l);
+        }
+        public static Range StartAt(Index start) => new Range(start, Index.End);
+        public static Range EndAt(Index end) => new Range(Index.Start, end);
+
+        // support method
+        public bool Equals(Range other) {
+            return
+                Start.Equals(other.Start) &&
+                End.Equals(other.End) &&
+                true;
+        }
+        public override bool Equals(object? value) {
+            if (value is Range v) {
+                return Equals(v);
+            } else {
+                return false;
+            }
+        }
+        public override int GetHashCode() {
+            return new CSharp8ForDotNetFramework.__hashcode_cs8()
+                .Combine(Start)
+                .Combine(End)
+                ;
+        }
+        public override string ToString() {
+            return $"{Start}..{End}";
+        }
+    }
+
+    namespace Runtime.CompilerServices {
+        static class RuntimeHelpers {
+            public static T[] GetSubArray<T>(T[] array, Range range) {
+                (int offset, int length) = range.GetOffsetAndLength(array.Length);
+                T[] ret = new T[length];
+
+                for (int iret = 0, isrc = offset; iret < length; iret++, isrc++) {
+                    ret[iret] = array[isrc];
+                }
+                return ret;
+            }
+        }
+
+    }
+}
+
+namespace CSharp8ForDotNetFramework {
+    // INTERNAL hash utility.
+    // usage:
+    //     public override int GetHashCode() {
+    //         return new CSharp8ForDotNetFramework.__hashcode_cs8()
+    //             .Combine(_field1)
+    //             .Combine(_field1)
+    //             .Combine(_field1)
+    //             .Final();
+    //     }
+    struct __hashcode_cs8 {
+        int _value;
+        public __hashcode_cs8 Combine<T>(T obj) where T : struct {
+            _value ^= _value << 13;
+            _value ^= _value >> 17;
+            _value ^= _value << 5;
+            _value ^= obj.GetHashCode();
+            return this;
+        }
+        public int Final() => _value;
+        public static implicit operator int(__hashcode_cs8 @this) {
+            return @this.Final();
+        }
     }
 }
