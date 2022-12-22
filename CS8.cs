b6dcdf7a9b3c8770788d14cdc6aa02b97292512e
okabe@home <okabe_m@hmi.aitech.ac.jp>
2022-12-22 23:28:33 +0900

    MemberNotNull系統のAttributeは CS9だったので移動

--- CS8.cs
+++ CS8.cs
@@ -59,28 +59,4 @@ namespace System.Diagnostics.CodeAnalysis {
             ParameterValue = parameterValue;
         }
     }
-    [System.AttributeUsage(
-        System.AttributeTargets.Method |
-        System.AttributeTargets.Property, AllowMultiple = true, Inherited = false)]
-    internal sealed class MemberNotNullAttribute : Attribute {
-        public string[] Members { get; }
-        public MemberNotNullAttribute(string member) :
-            this(new string[] { member }) { }
-        public MemberNotNullAttribute(string[] members) {
-            Members = members;
-        }
-    }
-    [System.AttributeUsage(
-        System.AttributeTargets.Method |
-        System.AttributeTargets.Property, AllowMultiple = true, Inherited = false)]
-    internal sealed class MemberNotNullWhenAttribute : Attribute {
-        public bool ReturnValue { get; }
-        public string[] Members { get; }
-        public MemberNotNullWhenAttribute(bool returnValue, string member) :
-            this(returnValue, new string[] { member }) { }
-        public MemberNotNullWhenAttribute(bool returnValue, string[] members) {
-            ReturnValue = returnValue;
-            Members = members;
-        }
-    }
 }
