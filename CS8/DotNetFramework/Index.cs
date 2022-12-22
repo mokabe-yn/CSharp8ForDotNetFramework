ac6b7205ae43037ee4accb2f631f31dcd6d5d44d
okabe@home <okabe_m@hmi.aitech.ac.jp>
2022-12-23 01:14:13 +0900

    Indexのテスト実装

--- CS8/DotNetFramework/Index.cs
+++ CS8/DotNetFramework/Index.cs
@@ -4,10 +4,25 @@ using System;
 namespace CS8 {
     [TestClass]
     public class Index {
-        static int[] A => new int[] { 1, 2, 3, 4, 5, 6 };
+        static int[] A => new int[] { 0, 1, 2, 3, 4, 5 };
         [TestMethod]
         public void LastElement() {
-            Assert.AreEqual(6, A[^1]);
+            Assert.AreEqual(5, A[^1]);
+        }
+        [TestMethod]
+        public void FrontElement() {
+            System.Index i = 2;
+            Assert.AreEqual(2, A[i]);
+        }
+
+        [TestMethod]
+        public void BackElement() {
+            System.Index i = ^2;
+            Assert.AreEqual(4, A[i]);
+        }
+        [TestMethod]
+        public void OutOfRange() {
+            Assert.ThrowsException<IndexOutOfRangeException>(() => A[^0]);
         }
     }
 }
