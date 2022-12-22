1461b0e66afb7a16ca32a6c4ed250a565dad5704
okabe@home <okabe_m@hmi.aitech.ac.jp>
2022-12-23 01:04:42 +0900

    Index実装 テストはまだ

--- CS8.cs
+++ CS8.cs
@@ -1,4 +1,8 @@
+#nullable enable
+
 // Nullable support Attributes
+using System.IO;
+
 namespace System.Diagnostics.CodeAnalysis {
     [System.AttributeUsage(
         System.AttributeTargets.Field |
@@ -58,5 +62,52 @@ namespace System.Diagnostics.CodeAnalysis {
         public DoesNotReturnIfAttribute(bool parameterValue) {
             ParameterValue = parameterValue;
         }
+    }
+}
+
+// Index, Range
+namespace System {
+    internal readonly struct Index : IEquatable<Index> {
+        private readonly int _value;
+        public Index(int value, bool fromEnd = false) {
+            if (value < 0) throw new ArgumentException(nameof(value));
+            _value = fromEnd ? value : ~value;
+        }
+
+        // properties
+        public int Value => IsFromEnd ? ~_value : _value;
+        public bool IsFromEnd => _value < 0;
+        static public Index Start => new Index(0, false);
+        static public Index End => new Index(0, true);
+
+        // core method
+        public int GetOffset(int length) {
+            if(IsFromEnd) {
+                return length - Value;
+            } else {
+                return _value;
+            }
+        }
+        static public Index FromStart(int value) => new Index(value, false);
+        static public Index FromEnd(int value) => new Index(value, true);
+
+        // support method
+        public bool Equals(Index other) {
+            return _value == other._value;
+        }
+        public override bool Equals(object? value) {
+            if (value is Index v) {
+                return Equals(v);
+            } else {
+                return false;
+            }
+        }
+        public override int GetHashCode() {
+            return _value.GetHashCode();
+        }
+        public override string ToString() {
+            return IsFromEnd ? $"^{Value}" : $"{_value}";
+        }
+        // operators
     }
 }
---
+++ CS8/DotNetFramework/Index.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CS8 {
+    [TestClass]
+    public class Index {
+        static int[] A => new int[] { 1, 2, 3, 4, 5, 6 };
+        [TestMethod]
+        public void LastElement() {
+            Assert.AreEqual(6, A[^1]);
+        }
+    }
+}
