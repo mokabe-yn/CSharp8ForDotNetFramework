4a91cde22a1752604a962c613f22645d9fa80f18
okabe@home <okabe_m@hmi.aitech.ac.jp>
2023-01-19 20:17:31 +0900

    refactor test

--- CS8/DotNetFramework/AsyncEnumerable.cs
+++ CS8/DotNetFramework/AsyncEnumerable.cs
@@ -7,27 +7,28 @@ using System.Threading.Tasks;
 namespace CS8 {
     [TestClass]
     public class AsyncEnumerable {
-        static async IAsyncEnumerable<int> Generator(int generator_id, int first_wait) {
-            await Task.Delay(first_wait);
-            yield return generator_id;
-            await Task.Delay(100);
-            yield return generator_id;
-            await Task.Delay(100);
-            yield return generator_id;
-        }
-        class LockedQueue {
-            readonly Queue<int> _q = new Queue<int>();
-            public void Add(int a) {
+        class LockedQueue<T> {
+            readonly Queue<T> _q = new Queue<T>();
+            public void Add(T a) {
                 lock (_q) {
                     _q.Enqueue(a);
                 }
             }
-            public Queue<int> Q => _q;
+            public Queue<T> Q => _q;
         }
 
         [TestMethod, Timeout(2000)]
         public void TestMethod1() {
-            var q = new LockedQueue();
+            static async IAsyncEnumerable<int> Generator(int generator_id, int first_wait) {
+                await Task.Delay(first_wait);
+                yield return generator_id;
+                await Task.Delay(100);
+                yield return generator_id;
+                await Task.Delay(100);
+                yield return generator_id;
+            }
+
+            var q = new LockedQueue<int>();
             Task t1 = Task.Run(async () => {
                 await foreach (var x in Generator(0, 0)) {
                     q.Add(x);
@@ -38,12 +39,13 @@ namespace CS8 {
                     q.Add(x);
                 }
             });
-            Task.WaitAll(new[] { t1, t2 });
+            Task.WaitAll(t1, t2);
 
             CollectionAssert.AreEqual(new[] { 0, 1, 0, 1, 0, 1 }, q.Q);
             //System.Threading.Tasks.Sources.ManualResetValueTaskSourceCore;
             //System.Runtime.CompilerServices.ConfiguredAsyncDisposable;
             //System.Runtime.CompilerServices.AsyncIteratorMethodBuilder
         }
+
     }
 }
